013811afc14a5489b30842629fc167c92d48c681
LEagle_eye05 <likhithnaik01@gmail.com>
2025-01-26 20:52:27 +0530

    ejs starting

--- solution.js
+++ solution.js
@@ -1,38 +1,32 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-function passwordCheck(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
-    userIsAuthorised = true;
-  }
-  next();
-}
-app.use(passwordCheck);
-
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+  const today = new Date();
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
 
-app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
+  // console.log(day);
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
   }
+
+  res.render("solution.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server running on port ${port}.`);
 });
