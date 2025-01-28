9c573fb87cfb21be95132d759c8a7896203551c7
LEagle_eye05 <likhithnaik01@gmail.com>
2025-01-28 23:54:08 +0530

    ejstags

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- solution.js
+++ solution.js
@@ -4,29 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  //Test code
-  // weekend:
-  // new Date("June 24, 2023 11:13:00");
-  // weekday:
-  // new Date("June 20, 2023 11:13:00");
-  const day = today.getDay();
-
-  // console.log(day);
-  let type = "a weekday";
-  let adv = "it's time to work hard";
-
-  if (day === 0 || day === 6) {
-    type = "the weekend";
-    adv = "it's time to have some fun";
-  }
-
-  res.render("solution.ejs", {
-    dayType: type,
-    advice: adv,
-  });
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<em>This is some em text</em>",
+  };
+  res.render("solution.ejs", data);
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
+  console.log(`Server is running on port ${port}`);
 });
