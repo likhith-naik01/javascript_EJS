9751de07f7acbdcdeb7188d07466c476b0747ac3
LEagle_eye05 <likhithnaik01@gmail.com>
2025-02-04 14:34:20 +0530

    Add files via upload

--- solution.js
+++ solution.js
@@ -1,21 +1,32 @@
 import express from "express";
 
 const app = express();
-const port = 4008;
-app.use(express.static("public"));
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+  const today = new Date();
+  //Test code
+  // weekend:
+  // new Date("June 24, 2023 11:13:00");
+  // weekday:
+  // new Date("June 20, 2023 11:13:00");
+  const day = today.getDay();
 
-app.get("/about", (req, res) => {
-  res.render("about.ejs");
-});
+  // console.log(day);
+  let type = "a weekday";
+  let adv = "it's time to work hard";
 
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("solution.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}.`);
 });
